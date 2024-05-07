bd9dfa1231df56a51f319ace36eb06aa6630fd42
Leif Johansson <leifjsunet.se>
2024-05-07 22:42:01 +0200

    template react app

---
+++ extra_script.py
@@ -0,0 +1,15 @@
+Import("env")
+import os
+
+def before_build_spiffs(source, target, env):
+    print("Building React App...")
+    env.Execute("cd web && npm run build")
+    print("React App built!")
+
+    print("Removing old SPIFFS image...")
+    env.Execute("rm -rf data")
+
+    print("Copying React App to SPIFFS...")
+    env.Execute("cp -r web/build data")    
+
+env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
